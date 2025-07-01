907e8617f435e72890ce4f5acddaf93fbf47e6ef
Awokakak <andrerahman285@gmail.com>
2025-07-01 22:37:52 +0700

    perbaikan dari louis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <script src="https://kit.fontawesome.com/your_kit_id.js" crossorigin="anonymous"></script>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="font-sans antialiased">
-    <div class="min-h-screen flex bg-gray-100">
-
-        <aside class="w-64 bg-white shadow-md">
-            @include('layouts.sidebar')
-        </aside>
-
-        {{-- Main Content --}}
-        <div class="flex-1 flex flex-col">
-            @include('layouts.navigation') 
-
+            <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -33,10 +27,10 @@
                 </header>
             @endisset
 
-            <main class="flex-1 p-4">
+            <!-- Page Content -->
+            <main>
                 {{ $slot }}
             </main>
         </div>
-    </div>
-</body>
+    </body>
 </html>
